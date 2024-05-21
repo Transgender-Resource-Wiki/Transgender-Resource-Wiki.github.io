0c7901af789b2f80d819a14fce0cd403b36f4267
Transgender-Resource-Wiki <transgenderwiki@protonmail.com>
2024-05-20 23:19:49 -0500

    refactor

--- src/scripts/SiteNav.tsx
+++ src/scripts/SiteNav.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import '../styles/ColoredSideBars.css';
 import '../styles/Hamburger.css';
 import '../styles/SiteNavBackground.css';
 
@@ -23,21 +24,28 @@ export default function SiteNav(){
     return (
 
         <html>
+          <div className="left-blue"></div>
+          <div className="left-pink"></div>
+          <div className="center-white"></div>
+          <div className="right-pink"></div>
+          <div className="right-blue"></div>
+          <div className="horz-dark-blue-0"></div>
+          <div className="horz-dark-blue-1"></div>
         
-        {toggled && (
-        <span>
+          {toggled && (
+          <span>
             <div className="bg-block"></div>
+            <div className="bg-gray-block"></div>
             <div className="bg-darken"></div>
-        </span>
-        )}
+          </span>
+          )}
     
 
-        <div onClick={() => setToggled(prevToggle => !prevToggle)} 
-             className="hamburger">
+          <div onClick={() => setToggled(prevToggle => !prevToggle)} className="hamburger">
             <span className="line-1"></span>
             <span className="line-2"></span>
             <span className="line-3"></span>
-        </div>
+          </div>
         </html>
     )
 }
